beeab6f837e538e8a78dd5e61769f6c53019b8f2
Code-lab-dev <fawassaka862@gmail.com>
2025-01-26 10:56:34 -0500

    Update BackButton.tsx

--- src/components/ui/BackButton.tsx
+++ src/components/ui/BackButton.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react"
 
-const BackButton = () => {
+export const BackButton = () => {
   const navigate = useNavigate();
 
   return (
@@ -8,9 +9,9 @@ const BackButton = () => {
       onClick={() => navigate(-1)}
       className="bg-luxury-gold text-luxury-black px-4 py-2 rounded-md hover:bg-white transition duration-300"
     >
-      ← Back
+     <ArrowLeft />
     </button>
   );
 };
 
-export default BackButton;
+
